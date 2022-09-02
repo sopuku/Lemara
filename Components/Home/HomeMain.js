d53376398d9380d234184db47ef5e889f1465d9d
sopuku <sopuku@gmail.com>
2022-09-02 11:55:07 +0300

    shanges to home page

--- Components/Home/HomeMain.js
+++ Components/Home/HomeMain.js
@@ -10,84 +10,92 @@ import {
   Flex,
   Link,
   Image,
-  Center,
+  Wrap,
   Stack,
-  Hide,
   Show,
 } from "@chakra-ui/react";
 
 export default function HomeMain() {
   return (
-    <VStack spacing="0" overflow="hidden">
-      <Flex
-        position="relative"
-        direction="column"
-        align="center"
-        justify="center"
-        w="100vw"
-        h="50rem"
-        px={{ base: "2rem", sm: "10%", lg: "20%" }}
-        mb={{ base: 0, md: "25rem", lg: "25rem", xl: "10rem" }}
+    <React.Fragment>
+      <Box
+        position="absolute"
         backgroundSize="cover"
         backgroundPosition="center"
         backgroundRepeat="no-repeat"
         backgroundImage="/images/background/bg1.jpg"
-        boxShadow="0 0 20px 10px grey"
-      >
-        <Heading color="white" fontSize="6xl" pb="2rem">
-          Jums reikalingos CNC detalės jau pakeliui
-        </Heading>
-        <Text color="white" fontSize="3xl">
-          Projektuojame ir gaminame tiek pavienes detales, jų prototipus, tiek
-          serijas. Išsirinkite tinkamas paslaugas, o jei turite klausimų,{" "}
-          <Link color="orange" href="/contacts">
-            susisiekite su mumis
-          </Link>
-          .
-        </Text>
-      </Flex>
-      <Show above="md">
-        <Box position="absolute" top="50rem">
-          <Values />
-        </Box>
-      </Show>
-      <Box
         w="100%"
-        h={["70rem", "70rem", "65rem", "65rem", "45rem"]}
-        align="center"
-        py="7rem"
-      >
-        <Stack
-          justify="center"
-          spacing="1rem"
+        h="50rem"
+        filter="auto"
+        brightness="50%"
+      ></Box>
+      <VStack spacing="0" overflow="hidden">
+        <Flex
+          position="relative"
+          direction="column"
           align="center"
-          direction={{ base: "column", xl: "row" }}
-          px={["2%", "2%", "5%", "10%", "10%", "15%"]}
+          justify="center"
+          w="100vw"
+          h="50rem"
+          px={{ base: "2rem", sm: "10%", lg: "20%" }}
+          mb={{ base: 0, md: "25rem", lg: "25rem", xl: "10rem" }}
+          boxShadow="0 0 20px 10px grey"
         >
-          <Image
-            src="/images/whyUs.jpg"
-            minW="40rem"
-            minH="25rem"
-            boxShadow="0 0 5px 1px "
-          />
-          <Box>
-            <Heading py="3rem">Kodėl turėtumėte pasirinkti mus?</Heading>
-            <Text fontSize="lg" align="justify">
-              Esame jauni ir ambicingi savo srities profesionalai. Nuolatos
-              plečiamės ir keliame darbuotojų kvalifikaciją. Visada užtikriname
-              aukščiausios kokybės produktą, kurį atliekame per trumpiausią
-              įmanomą laiką. Specializuojamės gaminant didelio tikslumo
-              reikalaujančius elementus automatinėms ir robotizuotoms gamybos,
-              apdirbimo linijoms, konvejeriams, tačiau atliekame ir kitus
-              darbus. nuo 2016 metų sėkmingai gaminame detales didelėms Lietuvos
-              ir užsienio kompanijoms.
-            </Text>
+          <Heading color="white" fontSize="6xl" pb="2rem">
+            Jums reikalingos CNC detalės jau pakeliui
+          </Heading>
+          <Text color="white" fontSize="3xl">
+            Projektuojame ir gaminame tiek pavienes detales, jų prototipus, tiek
+            serijas. Išsirinkite tinkamas paslaugas, o jei turite klausimų,{" "}
+            <Link color="orange" href="/contacts">
+              susisiekite su mumis
+            </Link>
+            .
+          </Text>
+        </Flex>
+        <Show above="md">
+          <Box position="absolute" top="50rem">
+            <Values />
           </Box>
-        </Stack>
-      </Box>
+        </Show>
+        <Box
+          w="100%"
+          h={["70rem", "70rem", "65rem", "65rem", "45rem"]}
+          align="center"
+          py="7rem"
+        >
+          <Stack
+            justify="center"
+            spacing="1rem"
+            align="center"
+            direction={{ base: "column", xl: "row" }}
+            px={["2%", "2%", "5%", "10%", "10%", "15%"]}
+          >
+            <Image
+              src="/images/whyUs.jpg"
+              minW="40rem"
+              minH="25rem"
+              boxShadow="0 0 5px 1px "
+            />
+            <Box>
+              <Heading py="3rem">Kodėl turėtumėte pasirinkti mus?</Heading>
+              <Text fontSize="lg" align="justify">
+                Esame jauni ir ambicingi savo srities profesionalai. Nuolatos
+                plečiamės ir keliame darbuotojų kvalifikaciją. Visada
+                užtikriname aukščiausios kokybės produktą, kurį atliekame per
+                trumpiausią įmanomą laiką. Specializuojamės gaminant didelio
+                tikslumo reikalaujančius elementus automatinėms ir robotizuotoms
+                gamybos, apdirbimo linijoms, konvejeriams, tačiau atliekame ir
+                kitus darbus. nuo 2016 metų sėkmingai gaminame detales didelėms
+                Lietuvos ir užsienio kompanijoms.
+              </Text>
+            </Box>
+          </Stack>
+        </Box>
 
-      <AboutUs />
-      <Feedback />
-    </VStack>
+        <AboutUs />
+        <Feedback />
+      </VStack>
+    </React.Fragment>
   );
 }

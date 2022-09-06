fa07a8790ed3c461ad1e293ae7fec90ac6d1a4c8
sopuku <sopuku@gmail.com>
2022-09-06 13:04:27 +0300

    added fonts and other schanges

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,8 @@ import React, { useState } from "react";
 import Layout from "../Components/Ui/Layout";
 import Navigation from "../Components/Ui/Navigation/Navigation";
 import Footer from "../Components/Ui/Footer";
-import theme from "../Components/theme";
-import "../Components/styles.css";
+import theme from "../Components/Ui/Theme/theme";
+import "../Components/Ui/Theme/styles.css";
 
 export const CurrentSettings = React.createContext(null);
 

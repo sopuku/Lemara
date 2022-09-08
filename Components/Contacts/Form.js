b1ed9458b718e3d8f68218d73f82c249a802c76e
sopuku <sopuku@gmail.com>
2022-09-08 20:50:42 +0300

    captcha added

--- Components/Contacts/Form.js
+++ Components/Contacts/Form.js
@@ -48,7 +48,7 @@ export default function Form(props) {
       return;
     }
     try {
-      const response = await fetch("/api/sendMessage", {
+      const response = await fetch("/api/verification", {
         method: "POST",
         body: JSON.stringify({
           captcha: captchaCode,

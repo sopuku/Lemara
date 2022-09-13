14b71121c827bb499b37f34590dd7bb889412db8
sopuku <sopuku@gmail.com>
2022-09-13 17:49:53 +0300

    google verification added

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,10 @@ export default function Document() {
     <Html>
       <Head>
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta
+          name="google-site-verification"
+          content="qppZOOy2pWf1pTQ4ViD_5nL_2V4xA3IP5BQCde6tBYc"
+        />
       </Head>
       <body>
         <Main />

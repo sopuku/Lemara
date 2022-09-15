a77ce979a485944f55e7f1afc86203a690a9ee35
sopuku <sopuku@gmail.com>
2022-09-15 16:47:25 +0300

    test

--- pages/privacy.js
+++ pages/privacy.js
@@ -7,6 +7,7 @@ import { Suspense } from "react";
 const PrivacyMain = dynamic(() => import("../Components/Privacy/PrivacyMain"), {
   suspense: true,
 });
+//
 export default function Privacy() {
   const texts = Texts();
   return (

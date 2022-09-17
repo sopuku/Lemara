022de2503b3ea43ef4a7098523735b5a8e53386f
sopuku <sopuku@gmail.com>
2022-09-17 20:44:21 +0300

    google analytic added

--- Components/Ui/DefaultPage.js
+++ Components/Ui/DefaultPage.js
@@ -42,7 +42,7 @@ export default function DefaultPage(props) {
           </Square>
         )}
         <VStack spacing="8">
-          <h1>{props.heading}</h1>
+          <Heading as="h1">{props.heading}</Heading>
           <Text
             maxW="40rem"
             lineHeight={{ md: "1.8" }}

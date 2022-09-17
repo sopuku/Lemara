f2d08ee4f4085429d068e1714a3b315d7393ed12
sopuku <sopuku@gmail.com>
2022-09-17 19:33:51 +0300

    google analytic added

--- pages/_document.js
+++ pages/_document.js
@@ -42,17 +42,22 @@ export default function Document() {
           name="keywords"
           content="CNC Tekinimas, Frezavimas, Degalių gamyba"
         />
+
+        <scrip
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-QPQYS0TKC7')`,
+          }}
+        />
       </Head>
       <body>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-QPQYS0TKC7"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-QPQYS0TKC7')
-        </script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/gtag/js?id=G-QPQYS0TKC7" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        ></noscript>
         <Main />
         <NextScript />
       </body>

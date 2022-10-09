c1b49b11df26aaa2ecef6fb079053c0c1a23dc3e
sopuku <sopuku@gmail.com>
2022-10-09 13:51:59 +0300

    career changes

--- Components/Career/CareerMain.js
+++ Components/Career/CareerMain.js
@@ -26,59 +26,61 @@ export default function CareerMain(props) {
       <Accordion allowToggle pt={{ sm: "1rem", md: "2rem", lg: "5rem" }}>
         {props.data.slices.map((job) => {
           return (
-            <AccordionItem key={job.id} maxW="60rem">
-              <AccordionButton
-                color={props.data.button_text_color}
-                bg={props.data.button_background_color}
-                _hover={{
-                  background: props.data.button_hover_background_color,
-                  color: props.data.button_hover_text_color,
-                }}
-                _expanded={{
-                  bg: props.data.button_expanded_background_color,
-                  color: props.data.button_expanded_text_color,
-                }}
-              >
-                <Container
-                  textAlign="left"
-                  px="0"
-                  fontSize="30"
-                  fontWeight="600"
+            job.primary.published && (
+              <AccordionItem key={job.id} maxW="60rem">
+                <AccordionButton
+                  color={props.data.button_text_color}
+                  bg={props.data.button_background_color}
+                  _hover={{
+                    background: props.data.button_hover_background_color,
+                    color: props.data.button_hover_text_color,
+                  }}
+                  _expanded={{
+                    bg: props.data.button_expanded_background_color,
+                    color: props.data.button_expanded_text_color,
+                  }}
                 >
-                  {job.primary.title}
-                </Container>
-                <AccordionIcon />
-              </AccordionButton>
+                  <Container
+                    textAlign="left"
+                    px="0"
+                    fontSize="30"
+                    fontWeight="600"
+                  >
+                    {job.primary.title}
+                  </Container>
+                  <AccordionIcon />
+                </AccordionButton>
 
-              <AccordionPanel
-                pb="4"
-                textAlign="left"
-                pl="3rem"
-                bg={props.data.expanded_background_color}
-                color={props.data.expanded_text_color}
-                roundedBottom="8"
-              >
-                <CareerList
-                  name={props.data.description_title}
-                  items={job.primary.description}
-                />
+                <AccordionPanel
+                  pb="4"
+                  textAlign="left"
+                  pl="3rem"
+                  bg={props.data.expanded_background_color}
+                  color={props.data.expanded_text_color}
+                  roundedBottom="8"
+                >
+                  <CareerList
+                    name={props.data.description_title}
+                    items={job.primary.description}
+                  />
 
-                <CareerList
-                  name={props.data.requirments_title}
-                  items={job.primary.requirments}
-                />
+                  <CareerList
+                    name={props.data.requirments_title}
+                    items={job.primary.requirments}
+                  />
 
-                <CareerList
-                  name={props.data.offer_title}
-                  items={job.primary.offer}
-                />
+                  <CareerList
+                    name={props.data.offer_title}
+                    items={job.primary.offer}
+                  />
 
-                <CareerList
-                  name={props.data.salary_title}
-                  items={job.primary.salary}
-                />
-              </AccordionPanel>
-            </AccordionItem>
+                  <CareerList
+                    name={props.data.salary_title}
+                    items={job.primary.salary}
+                  />
+                </AccordionPanel>
+              </AccordionItem>
+            )
           );
         })}
       </Accordion>
